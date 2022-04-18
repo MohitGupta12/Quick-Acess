d0dc5b61ade7509df5ae5b470dec89663f0d31f1
MohitGupta12 <mohit2412gupta@gmail.com>
2022-04-19 00:10:01 +0530

    add functionality of adding section (beta)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,39 @@
- 
+
+const modelContent = document.querySelector(".model_content")
+document.getElementById("add_btn").addEventListener('click', addCont);
+
+function addCont() {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add('container');
+    
+    const brac_name = document.createElement("div");
+    brac_name.classList.add('brac_name');
+    brac_name.innerText = "Web Dev";
+    
+    const links = document.createElement("div");
+    links.classList.add('links');
+
+
+    let a_link=`
+    <a href="#!" class="link_name" target="_blank"> facebook </a>   
+    `;
+    let link;
+    // link.innerHTML = a_link;
+
+    for (let i = 0; i < 4; i++) {
+
+        link = document.createElement("div");
+        link.classList.add('link');
+        link.insertAdjacentHTML('beforeend', a_link);
+
+        links.appendChild(link);
+    }
+
+
+
+    
+    newDiv.appendChild(brac_name);
+    newDiv.appendChild(links);
+
+    modelContent.appendChild(newDiv);
+}
